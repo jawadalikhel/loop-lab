010159e545604dfc95f1360b46d35f53fd38c8ae
Jawad Alikhel <jawadalikhel@Jawads-MacBook-Pro.local>
2018-09-11 15:06:09 -0500

    This is my lab from 9-11about loops

---
+++ js/app.js
@@ -0,0 +1,69 @@
+// Uncaught SyntaxError: missing ) after argument list
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// console.log("and they're always glad you came.");
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// console.log("You wanna be where everybody knows");
+// console.log("Your name.");
+
+
+// While Loops: reps reps
+
+let rep = 1;
+while(rep <= 100)
+{
+	console.log('Ginger chocolate honey patties');
+	rep++;
+}
+
+/*
+let num =0;
+while(num <= 10)
+{
+	console.log(num);
+	num++;
+}
+
+let x = 0;
+while(x <= 10)
+{
+	console.log('Current loop number is: ' + x);
+	x++;
+}
+
+
+
+for (let i=0; i<=10; i++)
+{
+	console.log(i);
+}
+
+
+
+for (let i = 7; i <= 635; i++)
+{
+	console.log(i);
+}
+
+
+
+let start = 0;
+const limit = 100;
+
+for(start; start <= limit; start++)
+{
+	console.log(start);
+}
+
+*/
+
+
+
+
+
+
+
